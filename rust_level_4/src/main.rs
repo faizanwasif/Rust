a26111d04f335fcc3aa791a4eb58c7b6e876a040
faizanwasif <faizanwasif102@gmail.com>
2024-05-19 14:46:17 +0500

    Option and Result

--- rust_level_4/src/main.rs
+++ rust_level_4/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    
+    // Option
+    let std1 = Student {
+        name: "Faizan".to_string(),
+        age: 22,
+        phone: None,
+    };
+
+    match std1.print_info() {
+        Ok(_) => println!("Student information printed successfully."),
+        Err(e) => println!("Failed to print student information: {}", e),
+    }
 }
 
 /* 
@@ -7,3 +17,25 @@ fn main() {
     2) Result
     3) Hashmaps
 */
+
+// Option
+struct Student {
+    name: String,
+    age: u8,
+    phone: Option<String>,
+}
+
+impl Student {
+    fn print_info(&self) -> Result<(), String> {
+        if self.name.is_empty() {
+            return Err("Name is empty".to_string());
+        }
+
+        let phone = match &self.phone {
+            Some(phone) => phone,
+            None => "No phone number",
+        };
+        println!("Name: {}, Age: {}, Phone: {}", self.name, self.age, phone);
+        Ok(())
+    }
+}
